b5b750ea666f5bddc30812e69df02721f5e9a476
jflute <dbflute@gmail.com>
2016-01-07 21:16:07 +0900

    upgrade LastaFlute

--- src/test/java/org/docksidestage/mylasta/MermaidlagLastaDocTest.java
+++ src/test/java/org/docksidestage/mylasta/MermaidlagLastaDocTest.java
@@ -15,7 +15,6 @@
  */
 package org.docksidestage.mylasta;
 
-import org.dbflute.utflute.core.document.DocumentGenerator;
 import org.docksidestage.unit.UnitMermaidlagTestCase;
 
 /**
@@ -24,7 +23,6 @@ import org.docksidestage.unit.UnitMermaidlagTestCase;
 public class MermaidlagLastaDocTest extends UnitMermaidlagTestCase {
 
     public void test_document() throws Exception {
-        DocumentGenerator generator = new DocumentGenerator();
-        generator.saveLastaDocMeta();
+        saveLastaDocMeta();
     }
 }
